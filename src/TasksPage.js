f10936747bdc533ba7590c5d4c574ba8408e6011
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-08-26 12:06:15 +0100

    Added TasksPage.js starter code

---
+++ src/TasksPage.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import TaskList from './TaskList';
+
+const TASK_STATUSES = ['Unstarted', 'In Progress', 'Completed'];
+
+const TasksPage = props => {
+    const renderTaskLists = () => {
+        const { tasks } = props;
+        return TASK_STATUSES.map(status => {
+            const statusTasks = tasks.filter(task => task.status === status);
+            return <TaskList key={status} status={status} tasks={statusTasks} />
+        })
+    }
+
+    return (
+        <div className="tasks">
+            <div className="task-lists">
+                {renderTaskLists}
+            </div>
+        </div>
+    )
+}
+
+export default TasksPage;
