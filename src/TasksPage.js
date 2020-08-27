4e7dce85f80539530127ef51282113100153de02
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-08-27 13:56:02 +0100

    Auto-Formatted TasksPage.js with prettier

--- src/TasksPage.js
+++ src/TasksPage.js
@@ -67,7 +67,7 @@ const TasksPage = props => {
                     </button>
             </div>
             {state.showNewCardForm && (
-                <form className = 'task-list-form' onSubmit = {createTaskHandler}>
+                <form className='task-list-form' onSubmit={createTaskHandler}>
                     <input
                         className='full-width-input'
                         onChange={titleChangeHandler}
