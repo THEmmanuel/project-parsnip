d0d5f901c6f5e5823875b5a5d87e7379d5a1f939
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-08-28 10:26:02 +0100

    Added createtask imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import TasksPage from './TasksPage';
+import { createTask } from './actions/index'
 
 const App = props => {
 
   const createTaskHandler = ({ title, description }) => {
-    props.dispatch({
-      type: 'CREATE_TASK',
-      payload: {
-        title,
-        description
-      }
-    })
+    props.dispatch(createTask({title, description}));
   }
 
 
   return (
     <div className="main-content">
-      <TasksPage tasks={props.tasks} createTaskHandler = {createTaskHandler} />
+      <TasksPage tasks={props.tasks} createTaskHandler={createTaskHandler} />
     </div>
   );
 }
