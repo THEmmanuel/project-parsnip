b24e8b0d0f75598819dd06129ba6de7452e3d539
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-09-08 11:46:16 +0100

    Removed imports and mockTasks from the reducer methods

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,4 @@
-import { uniqueId } from '../actions/index';
-
-const mockTasks = [
-    {
-        id: uniqueId(),
-        title: 'Learn Redux',
-        description: 'The store, actions, and reducers, oh my!',
-        status: 'In Progress',
-    },
-    {
-        id: uniqueId(),
-        title: 'Peace on Earth',
-        description: 'No big deal.',
-        status: 'In Progress',
-    },
-]
-
-const tasks = (state = { tasks: mockTasks }, action) => {
+const tasks = (state = { tasks: []}, action) => {
 
     if (action.type === 'CREATE_TASK') {
         return { tasks: state.tasks.concat(action.payload) }
