ed20aef4026cc40fb82a2c1bfc0701a9aaaedd5b
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-09-09 02:43:02 +0100

    Added initial state to index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
+const initialState = {
+    tasks: [],
+    isLoading: false,
+}
+
 const tasks = (state = { tasks: [] }, action) => {
-
-
     switch (action.type) {
         case 'CREATE_TASK': {
             return { tasks: state.tasks.concat(action.payload) }
@@ -20,26 +23,31 @@ const tasks = (state = { tasks: [] }, action) => {
 
         case 'FETCH_TASKS_SUCCEEDED': {
             return {
+                ...state,
+                isLoading: false,
                 tasks: action.payload.tasks,
             };
         }
 
         case 'CREATE_TASK_SUCCEEDED': {
             return {
+                ...state,
                 tasks: state.tasks.concat(action.payload.task)
             }
         }
 
         case 'EDIT_TASK_SUCCEEDED': {
             const { payload } = action;
+            const nextTasks = state.tasks.map(task => {
+                if (task.id === payload.task.id) {
+                    return payload.task;
+                }
+                return task;
+            })
             return {
-                tasks: state.tasks.map(task => {
-                    if (task.id === payload.task.id) {
-                        return payload.task;
-                    }
-                    return task;
-                })
-            }
+                ...state,
+                tasks: nextTasks,
+            };
         }
 
         default: {
